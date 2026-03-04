0f85e8f5d8af920b0db545e0ebe646b904611078
scrannah <hannah.timms@hotmail.co.uk>
2026-03-04 16:56:45 +0000

    8 direction driving

--- epuck.py
+++ epuck.py
@@ -1,6 +1,8 @@
 from controller import Robot
 import numpy as np
 import cv2
+import heapq
+import math
 
 robot = Robot()
 timestep = int(robot.getBasicTimeStep())
@@ -8,6 +10,12 @@ timestep = int(robot.getBasicTimeStep())
 # CONSTANTS / FINETUNING
 
 # DEVICES
+
+gps = robot.getDevice("gps")
+gps.enable(timestep)
+
+inertial_unit = robot.getDevice("inertial unit")
+inertial_unit.enable(timestep)
 
 camera = robot.getDevice("camera(1)")
 camera.enable(timestep)
@@ -18,6 +26,7 @@ leftMotor.setPosition(float("inf"))
 rightMotor.setPosition(float("inf"))
 leftMotor.setVelocity(0.0)
 rightMotor.setVelocity(0.0)
+
 min_area = 100
 max_area = 500000
 
@@ -25,7 +34,7 @@ max_speed = 6.28
 pixel_tolerance = 50  # how centered to the object
 
 # close enough for CNN using bounding box area
-cnn_area_frac = 0.35  # stop when target box covers x% of frame
+cnn_area_frac = 0.30  # stop when target box covers x% of frame
 area_confirm_frames = 3  # must be above threshold this many frames in a row
 
 height = camera.getHeight()
@@ -34,13 +43,20 @@ width = camera.getWidth()
 cnn_area_stop = cnn_area_frac * (width * height)  # how many pixels should we have in frame before we stop
 
 # stop centering jitter
-uncentre_confirm_frames = 5  # must be uncentred this many frames before we turn
+uncentre_confirm_frames = 5  # must be uncentered this many frames before we turn
 centre_confirm_frames = 2  # must be centred this many frames before we go forward
 
 lost = 0
 lost_frames = 5
 
-# PATHFINDING
+#  A* PATHFINDING
+
+planned_path = None  # path needs to be remembered outside of timestep
+current_path_cell = 0
+travelling = "recycle_point"
+
+angle_close_enough = 0.20
+waypoint_reached_distance = 0.03
 
 ps0 = robot.getDevice("ps0")
 ps1 = robot.getDevice("ps1")
@@ -49,44 +65,196 @@ ps0.enable(timestep)
 ps1.enable(timestep)
 ps2.enable(timestep)
 
-# PLAN ARENA OUT FOR A*
-
 arena_x = 3.0
 arena_y = 2.0
 
-nx = 60
-ny = 40
+nx = 60  # 60 cells wide
+ny = 40  # 40 cells height
 
 cell_x = arena_x / nx
 cell_y = arena_y / ny
 
-grid = [[0 for a in range()] for b in range(nx)]  # initialise all cells as free until otherwise said
+grid = [[0 for _ in range(ny)] for _ in range(nx)]  # initialise all cells as free until otherwise said
 
-for a in range(nx):
-    for b in range(ny):
-        if a == 0 or a == nx - 1 or b == 0 or b == ny - 1:
-            grid[a][b] = 1  # youre on the edge of the map, this is blocked space
+for ix in range(nx):
+    for iy in range(ny):
+        if ix == 0 or ix == nx - 1 or iy == 0 or iy == ny - 1:
+            grid[ix][iy] = 1  # on the edge of the map, this is blocked space
 
 static_occupied_cells = (
-        [(41, b) for b in range(9, 40)] +
-        [(a, b) for b in range(35, 40) for a in range(50, 55)] +
-        [(a, b) for b in range(24, 29) for a in range(55, 60)] +
-        [(a, b) for b in range(14, 19) for a in range(55, 60)]
-)
+        [(41, iy) for iy in range(9, 40)] +
+        [(40, iy) for iy in range(9, 40)] +
+        [(42, iy) for iy in range(9, 40)] +
+        [(41, 8), (40, 8), (42, 8)])
+# + [(a, b) for b in range(35, 40) for a in range(50, 55)]
+# + [(a, b) for b in range(24, 29) for a in range(55, 60)]
+# + [(a, b) for b in range(14, 19) for a in range(55, 60)]
 
-for a, b in static_occupied_cells:
-    grid[a][b] = 1  # extra walls and recycling points set to occupied
+dynamic_occupied_cells = ()  # insert obstacles found
+
+for ix, iy in static_occupied_cells:
+    grid[ix][iy] = 1  # extra walls and recycling points set to occupied
+
+
+def clamp(v, low, high):
+    # keep cells between min max cells. some objects may leak into occupied external cells
+    if v < low:
+        return low
+    if v > high:
+        return high
+    return v
+
+
+def gps_to_cell(x, y):
+    # shift x from [-arena_x/2, +arena_x/2] into [0, arena_x]
+    shifted_x = x + arena_x / 2  # map on x axis goes from -1.5 to +1.5, we are centred around 0
+    shifted_y = y + arena_y / 2  # -1 to + 1 on y axis
+
+    # convert from  meters to cells
+    ix = int(shifted_x / cell_x)
+    iy = int(shifted_y / cell_y)
+
+    # clamp indices so we never go outside the indexing, lists cannot be -1 and 60 doesnt exist in lists as they count from 0
+    ix = clamp(ix, 0, nx - 1)  # 60 rows in list space is 0-59
+    iy = clamp(iy, 0, ny - 1)
+
+    # return grid coordinate
+    return ix, iy
+
+
+def cell_to_gps(ix, iy):
+    left_edge_arena = -arena_x / 2.0  # arena index starts from bottom left edge
+    bottom_edge_arena = -arena_y / 2.0  # -1.5, -1 is index [0,0]
+
+    target_world_x = left_edge_arena + (
+            ix + 0.5) * cell_x  # start from bottom left, add ixy + half cell length to get centre of cell
+    target_world_y = bottom_edge_arena + (iy + 0.5) * cell_y  # then * by cell physical size to get GPS location
+
+    return target_world_x, target_world_y
+
+
+def manhattan(ix, iy):  # for 4 distance movement
+    # a and b are (ix, iy)
+    return abs(ix[0] - iy[0]) + abs(ix[1] - iy[1])  # abs means its positive regardless of sign
+
+
+def euclidean(neighbour_cell, goal_cell):
+    dx = goal_cell[0] - neighbour_cell[0]
+    dy = goal_cell[0] - neighbour_cell[0]
+    return math.sqrt(dx * dx + dy * dy)
+
+
+def get_neighbours(cell):
+    cx, cy = cell  # unpacking tuple
+    candidates = [
+        (cx + 1, cy),  # right
+        (cx - 1, cy),  # left
+        (cx, cy + 1),  # up
+        (cx, cy - 1),  # down
+        (cx + 1, cy + 1),  # diagonal up right
+        (cx + 1, cy - 1),  # diagonal down right
+        (cx - 1, cy + 1),  # diagonal up left
+        (cx - 1, cy - 1), ]  # diagonal down left
+    # all possible movements for 8 directions
+    neighbours = []
+    for x_candidate, y_candidate in candidates:  # check each candidate
+        if 0 <= x_candidate < nx and 0 <= y_candidate < ny:  # if cells are within cell list index [0-59]
+            if grid[x_candidate][y_candidate] == 0:  # and not occupied
+                neighbours.append((x_candidate, y_candidate))  # they are a neighbour to check
+    return neighbours
+
+
+def astar(start_cell, goal_cell):
+    open_heap = []
+    heapq.heappush(open_heap, (
+        0, start_cell))  # heapq.heappush(yourheaphere, youritemhere) my item is a tuple (f_score, start_cell)
+    came_from = {}  # dictionaries for these, came from needs t know the cell it cames from
+    g_score = {start_cell: 0}  # g score needs score for cell, its 0 at start
+    while open_heap:
+        f_score, current_cell = heapq.heappop(open_heap)  # smallest item popped off heap
+
+        if current_cell == goal_cell:
+            # print("goal reached")
+            path = [goal_cell]
+            while path[-1] != start_cell:  # while we are not at the start
+                path.append(came_from[path[-1]])  # getting the parent of the prior cells to form the path
+            path.reverse()  # reverse so we can follow
+            return path
+
+        for neighbour_cell in get_neighbours(current_cell):
+
+            # print("current:", current_cell, "neighbour:", neighbour_cell)
+            dx = neighbour_cell[0] - current_cell[0]  # check if we moved diagonally
+            dy = neighbour_cell[1] - current_cell[1]  # if these are both not 0, we did
+            step_cost = math.sqrt(2) if (dx != 0 and dy != 0) else 1.0  # if we moved diagonally step cost is square 2
+            tentative_g = g_score[
+                              current_cell] + step_cost  # we can only move in one direction, tentative g for neighbour is the current cell g score + 1
+
+            if tentative_g < g_score.get(neighbour_cell,
+                                         float(
+                                             "inf")):  # inf because it may be the first time we discover the cell .get(key, dafault value)
+                # this finds the best g score to reach cell, is the tentative g cheaper than recorded g
+                came_from[neighbour_cell] = current_cell  # update came from history for best neighbour cell
+                g_score[neighbour_cell] = tentative_g  # update g score for neighbour cell
+
+                h = euclidean(neighbour_cell, goal_cell)
+                f = tentative_g + h
+                heapq.heappush(open_heap, (f, neighbour_cell))
+    return None
+
+
+def drive_to_waypoint(target_world_x, target_world_y):
+    global leftSpeed, rightSpeed  # these need to be global so they can be used outside of function
+    position = gps.getValues()  # need to know where you are to move into cell space
+    x = position[0]
+    y = position[1]
+
+    robot_yaw = inertial_unit.getRollPitchYaw()[2]
+    vector_x = target_world_x - x  # how muych we need to vector to target
+    vector_y = target_world_y - y  # target subtract current location
+
+    distance_to_target = math.sqrt(vector_x ** 2 + vector_y ** 2)  # euclidean distance
+
+    if distance_to_target < waypoint_reached_distance:
+        leftMotor.setVelocity(0.0)
+        rightMotor.setVelocity(0.0)
+        return True  # at target
+
+    yaw_needed = math.atan2(vector_y, vector_x)
+    yaw_to_rotate = yaw_needed - robot_yaw
+    while yaw_to_rotate > math.pi:
+        yaw_to_rotate -= 2 * math.pi
+    while yaw_to_rotate < -math.pi:
+        yaw_to_rotate += 2 * math.pi
+    if abs(yaw_to_rotate) > angle_close_enough:  # turn to waypoint
+
+        if yaw_to_rotate > 0:  # turn right
+            leftSpeed = -(0.2 * max_speed)
+            rightSpeed = 0.2 * max_speed
+
+        else:  # turn left
+            leftSpeed = 0.2 * max_speed
+            rightSpeed = -(0.2 * max_speed)
+
+        return False  # not at target
+
+    else:  # forward to waypoint
+        leftSpeed = 0.2 * max_speed
+        rightSpeed = 0.2 * max_speed
+
+        return False  # not at target
+
 
 # STATE MACHINE, YOU NEED THIS THROUGH THE WHOLE PIPELINE
 
 state = "SEARCHING"
 locked_rect = None  # store outside of loop, reinitialise within loop when new target is needed
 area_ok_count = 0
-uncenter_count = 0
+uncentre_count = 0
 centre_count = 0
 
 
-# FUNCTIONS/MATHY BITS
+# FUNCTIONS/MATH BITS
 
 def get_opencv_image(camera_device):
     raw = camera_device.getImage()
@@ -109,14 +277,14 @@ def visualise(frame):
     cv2.waitKey(1)
 
 
-def bottom_y(rect):
+def bottom_y(rect):  # looks at the bottom which helps with what appears closest to robot
     x, y, w, h = rect
     return y + h
 
 
 def rect_centre(rect):
     x, y, w, h = rect
-    return (x + w / 2, y + h / 2)
+    return x + w / 2, y + h / 2
 
 
 def closest_to_locked(rects, locked):
@@ -184,11 +352,11 @@ while robot.step(timestep) != -1:  # remember its one big loop, the robot has no
             # Lock immediately on the closest object
             locked_rect = max(bounding_rects, key=bottom_y)
             area_ok_count = 0
-            uncenter_count = 0
-            center_count = 0
+            uncentre_count = 0
+            centre_count = 0
             lost = 0
             state = "APPROACHING"
-            print("LOCKED  APPROACHING")
+            print("LOCKED APPROACHING")
         else:
             # Keep scanning
             leftSpeed = 0.2 * max_speed
@@ -200,6 +368,7 @@ while robot.step(timestep) != -1:  # remember its one big loop, the robot has no
     # STATE: APPROACHING
 
     if state == "APPROACHING":
+
         # if we have detections this frame, update where the locked target is
         if len(bounding_rects) > 0 and locked_rect is not None:
             target_rect = closest_to_locked(bounding_rects, locked_rect)
@@ -217,16 +386,25 @@ while robot.step(timestep) != -1:  # remember its one big loop, the robot has no
                 rightMotor.setVelocity(rightSpeed)
                 continue
 
-            # spin until you find again or set as lost and retunr to searching
-
+            # lost is greater than lost frame limit
+            # spin until you find again or set as lost and return to searching
             leftSpeed = 0.15 * max_speed
             rightSpeed = -0.15 * max_speed
             leftMotor.setVelocity(leftSpeed)
             rightMotor.setVelocity(rightSpeed)
-            uncenter_count = 0
-            center_count = 0
+            uncentre_count = 0
+            centre_count = 0
             area_ok_count = 0
-            continue
+
+            if lost > 10:
+                state = "SEARCHING"  # youre mega lost give up and search again
+                locked_rect = None  # give up on that rect
+                leftSpeed = 0.0 * max_speed
+                rightSpeed = 0.0 * max_speed
+                leftMotor.setVelocity(leftSpeed)
+                rightMotor.setVelocity(rightSpeed)  # stop spinning to return to search
+
+            continue  # were lost so continue out of loop, re-enter back at approaching or searching
 
         # Use the locked rect for steering
         x, y, w, h = locked_rect
@@ -266,7 +444,7 @@ while robot.step(timestep) != -1:  # remember its one big loop, the robot has no
         elif centre_count >= centre_confirm_frames:
             # decide if close enough for CNN using bounding box area
             box_area = w * h
-            print(f"box area={box_area:.0f} threshold={cnn_area_stop:.0f}")
+            # print(f"box area={box_area:.0f} threshold={cnn_area_stop:.0f}")
 
             if box_area >= cnn_area_stop:
                 area_ok_count += 1
@@ -276,32 +454,84 @@ while robot.step(timestep) != -1:  # remember its one big loop, the robot has no
             if area_ok_count >= area_confirm_frames:
                 leftSpeed = 0.0
                 rightSpeed = 0.0
-                state = "CNN_CAPTURE"
                 print("Close enough, let's get a picture")
-                state = 'SEARCHING'
+                state = "PATHFIND"
+                travelling = "recycle_point"
+
             else:
                 # Approach forward
                 leftSpeed = 0.2 * max_speed
                 rightSpeed = 0.2 * max_speed
-                print("centred, going forward")
+                # print("centred, going forward")
+        else:
+            leftSpeed = 0.0  # mitigate stale speed if centre counts not met
+            rightSpeed = 0.0
 
-        leftMotor.setVelocity(leftSpeed)
-        rightMotor.setVelocity(rightSpeed)
-
-    # STATE: CNN CAPTURE
+            # STATE: CNN CAPTURE
 
     if state == "CNN_CAPTURE":
-        leftMotor.setVelocity(0.0)
-        rightMotor.setVelocity(0.0)
+        # print("CNN_CAPTURE READY")
+        state = "PATHFIND"
+        # if cnn = recycle point identifier:
 
-        print("CNN_CAPTURE READY")
-        break
-        # get picture
-        # pathfind
-        # reinitialise locked rect
+    if state == "PATHFIND":
 
-    # if state == "PATHFIND":
-    # PATHFIND
+        recycle_coord = (1.11, 0.89)  # metal
+        # recycle coord = the correct coord
+        world_reset = (0, 0)
+        if travelling == "home":
+            goal_world_x, goal_world_y = world_reset
+            goal_cell = gps_to_cell(goal_world_x, goal_world_y)
+
+        elif travelling == "recycle_point":
+            goal_world_x, goal_world_y = recycle_coord
+            goal_cell = gps_to_cell(goal_world_x, goal_world_y)
+
+        position = gps.getValues()  # need to know where you are to move into cell space
+        x = position[0]
+        y = position[1]
+
+        robot_cell = gps_to_cell(x, y)
+        # print(robot_cell)
+        # print(f"gps: ({x:.2f},{y:.2f}) cell:",robot_cell)
+        if planned_path is None:
+            planned_path = astar(robot_cell, goal_cell)
+            current_path_cell = 0
+            print("replanning from", robot_cell, "to", goal_cell)  # add this
+            print("path:", planned_path)
+
+            if planned_path is None:  # if a* cant find a path
+                leftMotor.setVelocity(0.0)
+                rightMotor.setVelocity(0.0)
+                print("no path found")
+                # maybe have some clear up of obstacles
+                continue
+
+        if current_path_cell >= len(planned_path):  # if we have finished the path
+            leftMotor.setVelocity(0.0)
+            rightMotor.setVelocity(0.0)
+            planned_path = None  # wipe old path so A* replans for next
+            current_path_cell = 0  # reset index
+
+            if travelling == "recycle_point":
+                travelling = "home"
+                state = "PATHFIND"  # go home next
+            elif travelling == "home":
+                travelling = "recycle_point"
+                state = "SEARCHING"  # back to looking for objects
+            continue
+
+        target_cell = planned_path[current_path_cell]
+
+        target_cell_x = target_cell[0]
+        target_cell_y = target_cell[1]
+
+        target_world_x, target_world_y = cell_to_gps(target_cell_x, target_cell_y)
+
+        reached = drive_to_waypoint(target_world_x, target_world_y)
+
+        if reached is True:
+            current_path_cell += 1  # increment after each waypoint is reached
 
     leftMotor.setVelocity(leftSpeed)
     rightMotor.setVelocity(rightSpeed)
