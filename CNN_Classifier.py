c640b5ba8eb3cc9ca7d690ea72702f33f20e5ff3
scrannah <hannah.timms@hotmail.co.uk>
2026-03-17 16:00:16 +0000

    classifier adding

--- CNN_Classifier.py
+++ CNN_Classifier.py
@@ -4,9 +4,22 @@ from torchvision import datasets, transforms
 from torchvision.models import resnet18
 
 
+class SmartTransform:  # smart transform to only crop images that need it
+    def __init__(self):
+        self.center_crop = transforms.CenterCrop(224)
+        self.resize = transforms.Resize((64, 64))
+
+    def __call__(self, frame):
+        w, h = frame.size
+        # Only crop if image is big enough
+        if min(w, h) >= 224:
+            frame = self.center_crop(frame)
+        frame = self.resize(frame)
+        return frame
+
+
 class RubbishClassifier:
     def __init__(self):
-
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.resnet18 = resnet18(weights=None)
         self.resnet18.load_state_dict(torch.load("path here"))
@@ -15,23 +28,20 @@ class RubbishClassifier:
 
         # Instantiate the model and move it to the device
         self.resnet18 = self.resnet18.to(self.device)
+        self.resnet18.eval()
 
         self.transform = transforms.Compose([
-            transforms.ToPILImage(),
+            SmartTransform(),
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])])
 
     def run_model(self, frame):
-
         transformed_frame = self.transform(frame)
         transformed_frame = transformed_frame.unsqueeze(0)  # add batch dim on even though im only passing 1 frame
         transformed_frame = transformed_frame.to(self.device)
-        self.resnet18.eval()
         with torch.no_grad():
-            prediction = self.resnet18(transformed_frame)
+            classification = self.resnet18(transformed_frame)
 
-        return prediction
-
-
+        return classification
--- epuck.py
+++ epuck.py
@@ -3,6 +3,11 @@ import numpy as np
 import cv2
 import heapq
 import math
+import torch
+import torch.nn as nn
+from torchvision import datasets, transforms
+from torchvision.models import resnet18
+from PIL import Image
 
 
 class RobotDevices:
@@ -20,6 +25,8 @@ class RobotDevices:
 
         self.camera = self.robot.getDevice("camera(1)")
         self.camera.enable(self.timestep)
+        self.height = self.camera.getHeight()
+        self.width = self.camera.getWidth()
 
         self.leftMotor = self.robot.getDevice("left wheel motor")
         self.rightMotor = self.robot.getDevice("right wheel motor")
@@ -498,6 +505,49 @@ class VisionSystem:
             return "APPROACHING", leftSpeed, rightSpeed
 
 
+class SmartTransform:  # smart transform to only crop images that need it
+    def __init__(self):
+        self.center_crop = transforms.CenterCrop(224)
+        self.resize = transforms.Resize((64, 64))
+
+    def __call__(self, frame):
+        w, h = frame.size
+        # Only crop if image is big enough
+        if min(w, h) >= 224:
+            frame = self.center_crop(frame)
+        frame = self.resize(frame)
+        return frame
+
+
+class RubbishClassifier:
+    def __init__(self):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.resnet18 = resnet18(weights=None)
+        self.resnet18.load_state_dict(torch.load("path here"))
+        in_features = self.resnet18.fc.in_features
+        self.resnet18.fc = nn.Linear(in_features, 3)  # change the last layer (fc) into a three classifier
+
+        # Instantiate the model and move it to the device
+        self.resnet18 = self.resnet18.to(self.device)
+        self.resnet18.eval()
+
+        self.transform = transforms.Compose([
+            SmartTransform(),
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])])
+
+    def run_model(self, frame):
+        transformed_frame = self.transform(frame)
+        transformed_frame = transformed_frame.unsqueeze(0)  # add batch dim on even though im only passing 1 frame
+        transformed_frame = transformed_frame.to(self.device)
+        with torch.no_grad():
+            classification = self.resnet18(transformed_frame)
+
+        return classification
+
+
 class RobotController:
     def __init__(self):
         self.devices = RobotDevices()
@@ -575,11 +625,14 @@ class RobotController:
         return "PATHFIND", 0.0, 0.0
 
     def handle_cnn_capture(self):
-        #get fram
-        #run classifier
-        #return output
-        #DECLARE OUTPUT AND COMPARE WITH SUPERVISOR
-        classification = 1
+        frame = self.camera.getImage()
+        frame = np.frombuffer(frame, dtype=np.uint8).reshape((self.height, self.width, 4))
+        frame = frame[:, :, :3]  # remove alpha channel
+        frame = Image.fromarray(frame)
+        classification = self.run_model(frame)
+        print(classification)
+        # DECLARE OUTPUT AND COMPARE WITH SUPERVISOR
+
 
         return "PATHFIND", 0.0, 0.0, classification  # return cnn classifier aswell 1 2 3
 
