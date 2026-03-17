6370fc6bb6a870b4a6de4058ea9422926fa08d0e
scrannah <hannah.timms@hotmail.co.uk>
2026-03-17 13:29:49 +0000

    classifier adding

--- CNN_Classifier.py
+++ CNN_Classifier.py
@@ -14,10 +14,12 @@ from torchvision.datasets import ImageFolder
 from sklearn.model_selection import train_test_split
 from collections import Counter
 
-class SmartTransform: # smart transform to only crop images that need it
+
+class SmartTransform:  # smart transform to only crop images that need it
     def __init__(self):
         self.center_crop = transforms.CenterCrop(224)
         self.resize = transforms.Resize((64, 64))
+
     def __call__(self, img):
         w, h = img.size
         # Only crop if image is big enough
@@ -25,51 +27,29 @@ class SmartTransform: # smart transform to only crop images that need it
             img = self.center_crop(img)
         img = self.resize(img)
 
+
 # Check if GPU is available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
-
 # Class names for dataset
 class_names = ['metal', 'wood', 'cardboard']
 
-class RubbishClassifier
-original_model = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
-print(original_model)
-in_features = original_model.fc.in_features
-original_model.fc = nn.Linear(in_features, 3) #change the last layer (fc) into a three classifier
 
-# Instantiate the model and move it to the device
-model=original_model.to(device)
+class RubbishClassifier:
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.1e-4)
+    def __init__(self):
+        self.resnet18 = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
+        self.in_features = resnet18.fc.in_features
+        resnet18.fc = nn.Linear(in_features, 3)  # change the last layer (fc) into a three classifier
 
-def classifier(model, dataloader, criterion, device):
-    model.eval()
-    running_loss = 0
-    correct = 0
-    total = 0
-    all_labels = []
-    all_predictions = []
+        # Instantiate the model and move it to the device
+        self.model = resnet18.to(device)
 
-    with torch.no_grad():
-        for images, labels in dataloader:
-            images = images.to(device)
-            labels = labels.to(device)
 
-            outputs = model(images)
-            loss = criterion(outputs, labels)
+    def run_model(self, resnet18, frame):
+        resnet18.eval
+        with torch.no_grad():
+            prediction = self.resnet18(frame)
 
-            running_loss += loss.item() * images.size(0)
-
-            _, predicted = torch.max(outputs, 1)
-            correct += (predicted == labels).sum().item()
-            total += labels.size(0)
-
-            all_labels.extend(labels.cpu().numpy())
-            all_predictions.extend(predicted.cpu().numpy())
-
-    epoch_loss = running_loss / total
-    accuracy = correct / total
-    return epoch_loss, accuracy, all_labels, all_predictions
+        return prediction
