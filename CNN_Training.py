8b22add99274cb35015478d0026c659f21ba0ba4
scrannah <hannah.timms@hotmail.co.uk>
2026-05-13 14:49:27 +0100

    cnn changes - formatting

--- CNN_Training.py
+++ CNN_Training.py
@@ -15,7 +15,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
 # Class names for dataset
-class_names = ['metal', 'wood', 'cardboard']
+class_names = ['cardboard', 'metal', 'wood']
 
 
 class DatasetWrapper(Dataset):
